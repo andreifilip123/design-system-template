b65631446b9d35840d4437ce8db5fb141196fc27
Andrei Filip <andreifilip123@gmail.com>
2022-09-13 09:16:43 +0300

    Add basic vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import typescript from '@rollup/plugin-typescript';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'Fruity Design System',
+      fileName: 'fruity-design-system',
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+      plugins: [typescript({ tsconfig: './tsconfig.json' })],
+    },
+  },
 })
