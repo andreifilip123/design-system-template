dcb2775807f47ae7f2f394502b4b3273874c2dc5
Andrei Filip <andreifilip123@gmail.com>
2022-09-13 09:16:25 +0300

    Add example components

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// components 
+import LoadingSpinner from "./components/LoadingSpinner/LoadingSpinner";
+
+// utilities
+import copyToClipboard from "./utilities/copyToClipboard";
+
+export const components = {
+  LoadingSpinner
+};
+
+export const utilities = {
+  copyToClipboard
+};
